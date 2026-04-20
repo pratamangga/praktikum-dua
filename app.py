a1e72cf1359792ea85ea29a51da6dd2a4d19a938
Angga Pratama <anggayogapratamaa@gmail.com>
2026-04-20 10:54:45 +0700

    Streamlit

---
+++ app.py
@@ -0,0 +1,47 @@
+import streamlit as st
+
+st.title("Sistem Rekomendasi Jurusan")
+st.write("Praktikum 2 Sistem Cerdas | Ryan - 2313010608, Johan - 2313010604, Angga - 2313010605")
+
+minat = st.selectbox(
+    "Minat :",
+    ["Matematika", "Kimia", "Fisika", "Sosial", "Bahasa", "Seni", "Teknologi", "Bisnis", "Olahraga"]
+)
+
+nilai = st.selectbox(
+    "Nilai :",
+    ["Tinggi", "Sedang", "Rendah"]
+)
+
+hobi = st.selectbox(
+    "Hobi :",
+    ["Menghitung", "Membaca", "Menggambar", "Menyanyi", "Trending", "Olahraga", "Lainnya", "Tidak ada"]
+)
+
+if st.button("Dapatkan Rekomendasi"):
+
+    if minat in ["Matematika", "Fisika", "Kimia"] and nilai == "Tinggi":
+        st.success("Kamu cocok dengan Jurusan Saintek")
+
+    elif minat in ["Sosial", "Bahasa", "Bisnis"] and nilai in ["Tinggi", "Sedang"]:
+        st.success("Kamu cocok dengan Jurusan Soshum")
+
+    elif minat == "Seni" or hobi in ["Menggambar", "Menyanyi"]:
+        st.success("Kamu cocok dengan Jurusan Seni / Desain")
+
+    elif minat == "Teknologi" and hobi == "Trending":
+        st.success("Kamu cocok dengan Jurusan Teknologi Informasi")
+
+    elif minat == "Olahraga" or hobi == "Olahraga":
+        st.success("Kamu cocok dengan Jurusan Olahraga")
+
+    else:
+        st.warning("Rekomendasi belum tersedia")
+
+# Fakta yang dapat dimasukkan kedalam Sistem :
+
+# 1. Mahasiswa memiliki minat pada bidangnya.
+# 2. Mahasiswa memiliki kemampuan akademiknya.
+# 3. Mahasiswa dapat memilih Saintek / Soshum.
+# 4. Nilai dapat mempengaruhi pilihan jurusannya.
+# 5. Hobi dapat menjadi pertimbangan tambahan.
